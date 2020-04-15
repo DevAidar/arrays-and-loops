79d9e3428fc0fc2c4e99d0b29ad031c4424a5aa8
Lime Peach <limepeach@LimePeach.local>
2020-04-15 14:13:04 -0700

    Finished the assignment!

--- main.js
+++ main.js
@@ -294,3 +294,23 @@ console.log("--------------------------------------------------\n4. A maximal su
 // empty), so the sum is zero:
 
 // getMaxSubSum([-1, -2, -3]) = 0
+
+let getMaxSubSum = (arr) => {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let sum = 0;
+        for (let j = i; j < arr.length; j++) {
+            sum += arr[j];
+            result = Math.max(result, sum);
+        }
+    }
+    return result;
+}
+
+console.log(getMaxSubSum([-1, 2, 3, -9]));
+console.log(getMaxSubSum([2, -1, 2, 3, -9]));
+console.log(getMaxSubSum([-1, 2, 3, -9, 11]));
+console.log(getMaxSubSum([-2, -1, 1, 2]));
+console.log(getMaxSubSum([100, -9, 2, -3, 5]));
+console.log(getMaxSubSum([1, 2, 3]));
+console.log(getMaxSubSum([-1, -2, -3]));
